eb9b2d5fb749d3afd06b9a6ab32e9bb0d5dfd34e
Sanika <shelakesanika370@gmail.com>
2026-02-19 23:29:38 +0530

    program of pattern

---
+++ Core_Java/Pattern/Program3.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1; i<=n; i++) {
+			for(int j=n; j>=1; j--) {
+				System.out.print(in);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
