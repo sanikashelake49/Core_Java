cc862dc0dfeaf296a3b5ea63ebbc935d9b7f1e40
Sanika <shelakesanika370@gmail.com>
2026-03-01 23:57:33 +0530

    program with string

--- Core_Java/String/WordCount.java
+++ Core_Java/String/WordCount.java
@@ -1,11 +1,3 @@
-package com.string;
-
-public class WordCount {
-  public static void main(String[] args) {
-	
-	  String str="Welcome to the git profile in thid branch all the string prog";
-	  //checking the count of the word in the sentence
-	  String word[]=str.split(" ");
-	  System.out.println("Word count is:"+word.length);
-}
-}
+String str="Welcome to the git profile in thid branch all the string prog";
+String word[]=str.split(" ");
+System.out.println("Word count is:"+word.length);
