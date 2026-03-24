f2f0c3768e21a3ef83e02e41eedd063f552562f8
Sanika <shelakesanika370@gmail.com>
2026-03-24 23:37:37 +0530

    implementation of private constructor

---
+++ Core_Java/CoreJava_Basic/Constructor/Main_Private_Constructor.java
@@ -0,0 +1,13 @@
+package com.Constructor;
+
+public class Main_Private_Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Private_Constructor pc=new Private_Constructor(1,"sanika");
+		pc.id=1;
+		
+
+	}
+
+}
