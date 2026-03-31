1aae6f09bd8870a6f3edde5b17fce1e11a387e87
sanikashelake49 <shelakesanika49@gmail.com>
2026-03-31 23:58:48 +0530

    Update Ascending_Array.java

--- Core_Java/Array/Ascending_Array.java
+++ Core_Java/Array/Ascending_Array.java
@@ -4,7 +4,7 @@ public class Ascending_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// ascending 
+		// ascending order 
        int arr[]= {8,7,6,9};
       int max=arr[0];
       for(int i=1;i<arr.length;i++) {
