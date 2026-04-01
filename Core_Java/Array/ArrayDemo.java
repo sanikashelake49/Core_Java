0e72045933ef25af8c028764260ff46cb437946a
sanikashelake49 <shelakesanika49@gmail.com>
2026-04-02 00:00:01 +0530

    Update ArrayDemo.java

--- Core_Java/Array/ArrayDemo.java
+++ Core_Java/Array/ArrayDemo.java
@@ -9,7 +9,7 @@ public class ArrayDemo {
 		
 		//simple array print if we know its size
 		int arr[]= {1,2,3,4,5,6};
-		 for(int i=0;i<arr.length;i++)
+		  for(int i=0;i<arr.length;i++)
 		{   //array print by the index
 			 System.out.println(arr[i]);
 			//i++;
