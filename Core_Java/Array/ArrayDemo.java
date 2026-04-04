76d03a88404eaad32f0281a9032744e1f6bfd291
sanikashelake49 <shelakesanika49@gmail.com>
2026-04-04 23:05:11 +0530

    Update ArrayDemo.java

--- Core_Java/Array/ArrayDemo.java
+++ Core_Java/Array/ArrayDemo.java
@@ -11,7 +11,7 @@ public class ArrayDemo {
 		int arr[]= {1,2,3,4,5,6};
 		  for(int i=0;i<arr.length;i++)
 		{   //array print by the index
-			 System.out.println(arr[i]);
+			  System.out.println(arr[i]);
 			//i++;
 		}
 		 System.out.println("the success");
