299687be8ee2e601a243b9af2a9ee60f8e1c1e31
Sanika <shelakesanika370@gmail.com>
2026-04-18 17:23:02 +0530

    Finding mid element of given Array

---
+++ Core_Java/Array/midArray.java
@@ -0,0 +1,21 @@
+package com.array;
+
+
+public class midArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,4,5,12,3};
+		//int midIndex=arr.length/2;
+		//int midElement=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i==arr.length/2)
+			{
+				System.out.println( arr[i]);
+		}
+		}
+      //System.out.println("midElement is "+midElement);
+	}
+
+}
