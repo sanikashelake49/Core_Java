989478cdfad1ec1cf8f3fdd297e09461a8a799b2
sanikashelake49 <shelakesanika49@gmail.com>
2026-05-07 18:08:24 +0530

    Update ArrayDemo.java

--- Core_Java/Array/ArrayDemo.java
+++ Core_Java/Array/ArrayDemo.java
@@ -6,7 +6,7 @@ public class ArrayDemo {
 
 	public static void main(String[] args) {
 	     // TODO Auto-generated method stub
-		
+		   
 		//simple array print if we know its size
 		int arr[]= {1,2,3,4,5,6};
 		  for(int i=0;i<arr.length;i++)
